c461ef3a3c586892cabd1f0372b46bc6435c7bd5
trk345 <tawhidkhan3@gmail.com>
2025-01-22 20:38:35 +0600

    fixed deploy issue

--- react-app/src/main.jsx
+++ react-app/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="Portfolio-Website">
       <App />
     </BrowserRouter>
   </StrictMode>,
